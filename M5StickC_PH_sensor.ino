60af866d2b2fc0c6288dfa5468a96138910bbeed
Carlos Orts <mcorts69@gmail.com>
2020-06-24 23:11:58 +0200

    MQTT messaging

--- M5StickC_PH_sensor.ino
+++ M5StickC_PH_sensor.ino
@@ -1,15 +1,34 @@
 #include <M5StickC.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include "settings.h"
 
-const int analogInPin = 26; 
+// pH variables
+const int analogInPin = 3 6; 
 int sensorValue = 0; 
 unsigned long int avgValue; 
 float b;
 int buf[10],temp;
 
+// Communication variables
+WiFiClient espClient;
+PubSubClient clientMqtt(espClient);
+String mqttcommand = String(14);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
 void setup() {
-//  Serial.begin(9600);
+  Serial.begin(115200);
   // Initialize the M5StickC object
   M5.begin();
+  pinMode(M5_BUTTON_HOME, INPUT);
+
+  // Wifi
+  setup_wifi();
+  clientMqtt.setServer(mqtt_server, mqtt_port);
+  clientMqtt.setCallback(callback);
 
   // Lcd display
   M5.Lcd.begin();
@@ -31,7 +50,71 @@ void setup() {
   M5.Lcd.setTextSize(3);
 }
 
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("[MQTT]Incomming message (");
+  Serial.print(topic);
+  Serial.print(") ");
+  mqttcommand = "";
+  for (int i = 0; i < length; i++) {
+    mqttcommand += (char)payload[i];
+  }
+  Serial.print(mqttcommand);
+  Serial.println();
+  // Switch on the LED if an 1 was received as first character
+  if (mqttcommand == "LightOn") {
+    Serial.println("LightOn");
+  } 
+}
+
+void reconnect() {
+  Serial.print("[MQTT]Intentando conectar a servidor MQTT... ");
+  // Loop until we're reconnected
+  while (!clientMqtt.connected()) {
+    Serial.print(".");
+    // Attempt to connect
+    if (clientMqtt.connect(mqtt_id)) { // Ojo, para más de un dispositivo cambiar el nombre para evitar conflicto
+      Serial.println("");
+      Serial.println("[MQTT] connecting server...");
+      // Once connected, publish an announcement...
+      clientMqtt.publish(mqtt_sub_topic_healthcheck, "starting");
+      // ... and subscribe
+      clientMqtt.subscribe(mqtt_sub_topic_operation);
+    } else {
+      Serial.print("[MQTT]Error, rc=");
+      Serial.print(clientMqtt.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void loop() {
+  if (!clientMqtt.connected()) {
+    reconnect();
+  }
+  clientMqtt.loop();
+  
   for(int i=0;i<10;i++) 
   { 
     buf[i]=analogRead(analogInPin);
@@ -72,4 +155,10 @@ void loop() {
   M5.Lcd.setTextColor(BLACK);
   M5.Lcd.setCursor(30,50);
   M5.Lcd.print(phValue);
+
+  if(digitalRead(M5_BUTTON_HOME) == LOW){
+      snprintf (msg, 6, "%2.1f", phValue);
+      Serial.print("[MQTT] Sending pH: ");
+      clientMqtt.publish(mqtt_pub_topic_ph, msg);
+  }
 }
---
+++ settings.h
@@ -0,0 +1,12 @@
+// WiFi Configuration
+const char* ssid = "";
+const char* password = "";
+
+// MQTT Cibfiguration
+const char* mqtt_server = "192.168.1.114";
+const int mqtt_port = 1883;
+const char* mqtt_id = "aquarium-ph";
+const char* mqtt_sub_topic_healthcheck = "/home/aquarium-ph";
+const char* mqtt_pub_topic_ph = "/home/aquarium-ph/ph";
+const char* mqtt_sub_topic_operation = "/home/aquarium-ph/operation";
+
